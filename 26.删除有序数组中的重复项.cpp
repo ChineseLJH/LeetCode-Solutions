5c131cf2047e7d228c0ac121cd822b19b017d417
ChineseLJH <160102672+ChineseLJH@users.noreply.github.com>
2025-09-08 19:23:25 +0800

    完成 26.删除有序数组中的重复项

---
+++ 26.删除有序数组中的重复项.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=cpp
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int num=0,n=nums.size();
+        if(n==0) return 0;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[num]!=nums[i])
+            {
+                num++;
+                nums[num]=nums[i];
+            }
+        }
+        return num+1;
+    }
+};
+// @lc code=end
+
